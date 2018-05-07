314cc2c0581126fb618c637037d1304c3cb2ac52
Kitsun9 <luis.caire@live.com>
2018-05-07 03:51:43 -0700

    unicode encode error

--- scrape.py
+++ scrape.py
@@ -3,18 +3,41 @@ from bs4 import BeautifulSoup
 from urllib.request import urlopen
 
 
+
+
+
+
+
+def writeResults(txt):
+    txt = txt.strip()
+
+    txt = str(lineCounter) + "     " + txt
+    textFile.write(txt)
+
 textFile = open("textFile.txt", 'w')
+lineCounter = 0
+
+urlList = ["http://liquipedia.net/starcraft2/Marine_(Legacy_of_the_Void)",
+           "http://liquipedia.net/starcraft2/Banshee_(Legacy_of_the_Void)",
+           "http://liquipedia.net/starcraft2/Bunker_(Legacy_of_the_Void)",
+           "http://liquipedia.net/starcraft2/Starport_(Legacy_of_the_Void)"]
+
+for urlAddress in urlList:
+    page = urlopen(urlAddress)
+    print("######################################################")
+    i = 0
 
 
-page = urlopen("http://starcraft.wikia.com/wiki/List_of_StarCraft_II_units")
 
+    soup = BeautifulSoup(page, 'html.parser')
 
+    name_box = soup.findAll("div", attrs="infobox-cell-2")
+    for name in name_box:
+        i += 1
 
+    for number in range(0, i):
+        writeResults(name_box[number].text)
 
-
-soup = BeautifulSoup(page, 'html.parser')
-
-print(soup.a)
 
 
 #textFile.write(soup.get_text())
@@ -23,6 +46,7 @@ print(soup.a)
 
 
 textFile.close()
+
 
 
 
